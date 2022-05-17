9081fb98d815bde979c35bb69eb80ad26e1433f2
Alberto Paredes Rivas <alberto.paredes.bs@gmail.com>
2022-05-16 19:57:15 -0500

    Fetching users from external source and searching feature complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            monsters: [],
+            searchField: ""
+        };
+        console.log("constructor()");
+    }
+    
+    async componentDidMount(){
+        console.log("componentDidMount()");
+        try {
+            let response = await fetch("https://jsonplaceholder.typicode.com/users");
+            let users = await response.json();
+            this.setState(() => (
+                {monsters: users}
+                ), 
+                () => console.log(this.state)
+            );
+        } catch (error) {
+            console.log(error);
+        }
+        }
+        
+    render(){
+        console.log("render()");
+        let searchResult = this.state.monsters.filter(monster => {
+            return monster.name.toLowerCase().includes(this.state.searchField);
+        }); 
+        return (
+            <div className="App">
+                <input 
+                    className="search-box" 
+                    type="search" 
+                    placeholder="search monsters"
+                    onChange={event => {
+                        let searchField = event.target.value;
+                        this.setState(() => {
+                            return {searchField}
+                        });
+                    }}
+                />
+                {
+                    searchResult.map((monster) => {
+                        return <div key={monster.id}><h1>{monster.name}</h1></div>;
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
